d0cbaf6e76c7ca97b8892f8d7668bb280c34acec
Jacob Feldman <jacobgfeld@gmail.com>
2018-10-19 19:03:56 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(array)
   i = 0
   collection = []
   while i < array.size
-    collection << yeild(array[i])
+    collection << yield(array[i])
     i += 1
   end
   return collection
